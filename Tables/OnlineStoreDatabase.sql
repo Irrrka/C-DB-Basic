0dda749efc82f2407ad14aace97bc64a3a0336a2
Irrrka <irrrka@gmail.com>
2017-02-02 14:32:27 +0200

    Create OnlineStoreDatabase.sql

---
+++ Tables/OnlineStoreDatabase.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Cities
+(
+CityID INT PRIMARY KEY,
+Name VARCHAR(50)
+)
+
+CREATE TABLE Customers
+(
+CustomerID INT PRIMARY KEY,
+Name VARCHAR(50),
+Birthday DATE,
+CityID INT,
+CONSTRAINT FK_Customers_Cities FOREIGN KEY (CityID) REFERENCES Cities (CityID)
+)
+
+CREATE TABLE ItemTypes
+(
+ItemTypeID INT PRIMARY KEY,
+Name VARCHAR(50)
+)
+
+CREATE TABLE Items
+(
+ItemID INT PRIMARY KEY,
+Name VARCHAR(50),
+ItemTypeID INT,
+CONSTRAINT FK_Item_ItemTypes FOREIGN KEY (ItemTypeID) 
+REFERENCES ItemTypes (ItemTypeID)
+)
+
+
+CREATE TABLE Orders
+(
+OrderID INT PRIMARY KEY,
+CustomerID INT,
+CONSTRAINT FK_Orders_Customers FOREIGN KEY (CustomerID) 
+REFERENCES Customers (CustomerID)
+)
+
+	
+CREATE TABLE OrderItems
+(
+OrderID INT,
+ItemID INT,
+PRIMARY KEY (OrderID, ItemID),
+CONSTRAINT FK_OrderItems_Items FOREIGN KEY (ItemID) REFERENCES Items (ItemID),
+CONSTRAINT FK_OrderItems_Orders FOREIGN KEY (OrderID) REFERENCES Orders (OrderID),
+)
